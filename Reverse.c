830a14e969e8a9a106bd841983384cd67fcf5167
frozent <s.a.haridhakshini@gmail.com>
2017-04-04 10:50:51 -0700

    Add files via upload

---
+++ Reverse.c
@@ -0,0 +1,16 @@
+// To reverse an integer.
+#include<stdio.h>
+int main()
+{
+int n, reverse=0, rem;
+printf("Enter an integer: ");
+scanf("%d",&n);
+while(n!=0)
+{
+rem=n%10;
+reverse = reverse*10 + rem;
+n=n/10;
+}
+printf(" The reversed number is %d \n",reverse);
+return 0;
+}
